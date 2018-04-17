744d4b9a1b6c882bcbd7450403634d74a1cb112b
Ruslan Khuzin <huzinruslan@mail.ru>
2018-04-17 20:30:55 +0600

    Все, вот что я хотел
    Теперь дальше тебе по списку организаций на главной, плюс о бюджете вкладку оживить, сделать страницы. На странице эбаут тоже нужно вкладки оживить. Я дальше буду теперь работать по советнику.

---
+++ public/front/js/hightcharts_custom.js
@@ -0,0 +1,63 @@
+﻿jQuery(document).ready(function($) {
+Highcharts.chart('diagram', {
+
+    title: {
+        text: 'Хронологический график '
+    },
+
+    subtitle: {
+        text: 'Школа 22'
+    },
+
+    yAxis: {
+        title: {
+            text: 'Суммы'
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    },
+
+    plotOptions: {
+        series: {
+            label: {
+                connectorAllowed: false
+            },
+            pointStart: 2010
+        }
+    },
+
+    series: [{
+        name: 'Питание',
+        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+    }, {
+        name: 'Канц. товары',
+        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+    }, {
+        name: 'Ремонт',
+        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+    }, {
+        name: 'Социальные нужды',
+        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+    }
+    ],
+
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'bottom'
+                }
+            }
+        }]
+    }
+
+});
+});
